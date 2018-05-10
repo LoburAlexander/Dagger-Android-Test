727ed83634ddc0319e0c256eff9361de7d4a628e
Alexander Lobur <alex.lobur@yellow.id>
2018-05-10 13:13:42 +0300

    Added logging.

--- app/src/main/java/com/testdagger/activity/MainActivity.java
+++ app/src/main/java/com/testdagger/activity/MainActivity.java
@@ -1,16 +1,32 @@
 package com.testdagger.activity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import com.testdagger.R;
 import com.testdagger.databinding.ActivityMainBinding;
 import com.testdagger.viewmodel.MainViewModel;
 
+import javax.inject.Inject;
+
+import timber.log.Timber;
+
 public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBinding> {
+
+    @Inject
+    Resources mResources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Timber.d("Activity hash: %d, view model hash: %d", hashCode(), mViewModel.hashCode());
+        Timber.d("Resources hash: %d", mResources.hashCode());
+
+        mBinding.vTvMessage.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+        });
     }
 
 
